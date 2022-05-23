a3f4698cd9457872fb86d3048772cc0b17b268d4
sriharika13 <56622258+sriharika13@users.noreply.github.com>
2022-05-23 21:59:35 +0530

    Create Ques_1.py

---
+++ Programming Skills/Day1/Ques_1.py
@@ -0,0 +1,9 @@
+#Average Salary Excluding the Minimum and Maximum Salary
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        minn=min(salary)
+        maxx= max(salary)
+        print(minn)
+        print(maxx)
+        a=(sum(salary)-(minn+maxx))/(len(salary)-2)
+        return a
